4e53f361818d10e22827e29526e6cdb4b3d22480
KRP <kunal.r.rajpurohit@gmail.com>
2019-06-22 16:36:05 -0700

    enabling cors on heroku server

--- index.js
+++ index.js
@@ -102,6 +102,14 @@ var endpoints = [
 	}
 ];
 
+app.all('*', function(req, res, next) {
+     var origin = req.get('origin'); 
+     res.header('Access-Control-Allow-Origin', origin);
+     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     res.header('Access-Control-Allow-Headers', 'Content-Type');
+     next();
+});
+
 
 
 endpoints.forEach(function(endpoint){
